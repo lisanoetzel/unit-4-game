8649cb1ccbbb0cf92cbcf266d0569227c384df11
Mona Lisa Mameli <lisa.noetzel@gmail.com>
2019-08-10 09:57:56 -0400

    altered ids and functions in html and JS respectively

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,34 +1,45 @@
 //This loads HTML, CSS before JavaScript & jQuery
 $(document).ready(function(){
-    //This generates random numbers from 19 - 120
-        var randomNumber = Math.floor(Math.random()*102 + 19)
-            //console.log("this generates a random number:" + randomNumber);
-    //This displays the random number on the screen
+
+    //Global variables for user score, wins & losses
+    var userScore = 0;
+    var wins = 0;
+    var losses = 0;
+
+    //This is the game start function
+    $('.btn-block').on('click', function() {
+
+        //This is the initial value of each gem
+        var emeraldNumber = 0;
+        var opalNumber = 0;
+        var rubyNumber = 0;
+        var tigerEyeNumber = 0;
+
+         //This generates random numbers from 19 - 120
+         var randomNumber = Math.floor(Math.random()*102 + 19)
+            console.log("this generates a random number:" + randomNumber);
+        
+        //This displays the random number on the screen
         $('#scoreToTie').text(randomNumber);
-    //This generates random numbers from 1-12 for each of the four gems
-        var randomEmeraldNumber = Math.floor(Math.random()*12 + 1)
-        var randomOpalNumber = Math.floor(Math.random()*12 + 1)
-        var randomRubyNumber = Math.floor(Math.random()*12 + 1)
-        var randomTigerEyeNumber = Math.floor(Math.random()*12 + 1)
-            //console.log("These are random numbers:" + randomEmeraldNumber);  
-    //Variables for user score, wins & losses
-        var userInitialScore = 0;
-        var wins = 0;
-        var losses = 0;
-    //This displays the wins or losses on the screen
-        $('#wins').text(wins);
-        $('#losses').text(losses);
-    //This is the game function
-        function game () {
-            randomNumber = Math.floor(Math.random()*102 + 19)
-            $('#scoreToTie').text(randomNumber);
-            randomEmeraldNumber = Math.floor(Math.random()*12 + 1)
-            randomOpalNumber = Math.floor(Math.random()*12 + 1)
-            randomRubyNumber = Math.floor(Math.random()*12 + 1)
-            randomTigerEyeNumber = Math.floor(Math.random()*12 + 1)
-            userInitialScore = 0;
-            $('#userFinalScore').text(userInitialScore);
-        }
+
+        //This displays the wins or losses on the screen & the userScore
+        $('#winScore').text(wins);
+        $('#lossScore').text(losses);
+        $('#userScore').text(userScore);
+
+        //This generates random numbers from 1-12 for each of the four gems
+        emeraldNumber = Math.floor(Math.random()*12 + 1)
+            console.log("These are random numbers for the emerald:" + emeraldNumber); 
+        randomOpalNumber = Math.floor(Math.random()*12 + 1)
+        randomRubyNumber = Math.floor(Math.random()*12 + 1)
+        randomTigerEyeNumber = Math.floor(Math.random()*12 + 1)
+        userInitialScore = 0;
+        
+
+    }
+   
+     
+    
     //This is the winner's notice
         function youWin() {
             alert("You have won!");
