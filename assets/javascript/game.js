e6007b34787887133072f5a21750f3102c16d975
Mona Lisa Mameli <lisa.noetzel@gmail.com>
2019-08-10 17:38:17 -0400

    game works but the reset happens only when you click the start button

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,97 +2,65 @@
 $(document).ready(function(){
 
     //Global variables for user score, wins & losses
-    var userScore = 0;
-    var wins = 0;
-    var losses = 0;
+        var userScore = 0;
+        var wins = 0;
+        var losses = 0;
 
-    //This is the game start function
-    $('.btn-block').on('click', function() {
-
-        //This is the initial value of each gem
+    //This is the initial value of each gem
         var emeraldNumber = 0;
         var opalNumber = 0;
         var rubyNumber = 0;
         var tigerEyeNumber = 0;
 
-         //This generates random numbers from 19 - 120
-         var randomNumber = Math.floor(Math.random()*102 + 19)
-            console.log("this generates a random number:" + randomNumber);
-        
-        //This displays the random number on the screen
-        $('#scoreToTie').text(randomNumber);
+    //This is the game start function / on click listener for the Start Button  
+    $('.btn-block').on('click', function() {
 
-        //This displays the wins or losses on the screen & the userScore
+    //This generates the computer's random numbers from 19 - 120 
+        var randomNumber = Math.floor(Math.random()*102 + 19)
+            console.log("this generates a random number:" + randomNumber);
+
+    //This displays the scores for the number to tie, wins, losses, and an alert
+        $('#scoreToTie').text(randomNumber);
         $('#winScore').text(wins);
         $('#lossScore').text(losses);
-        $('#userScore').text(userScore);
+        $('#alert').empty();
 
-        //This generates random numbers from 1-12 for each of the four gems
+    //This generates random numbers from 1-12 for each of the four gems
         emeraldNumber = Math.floor(Math.random()*12 + 1)
-            console.log("These are random numbers for the emerald:" + emeraldNumber); 
-        randomOpalNumber = Math.floor(Math.random()*12 + 1)
-        randomRubyNumber = Math.floor(Math.random()*12 + 1)
-        randomTigerEyeNumber = Math.floor(Math.random()*12 + 1)
-        userInitialScore = 0;
-        
+            console.log("These are random numbers for the emeralds:" + emeraldNumber)
+        var emeralds = $('#emeralds');
+            emeralds.attr("data-gemValue", emeraldNumber);
 
-    }
-   
-     
-    
-    //This is the winner's notice
-        function youWin() {
-            alert("You have won!");
-            wins++;
-            $('#wins').text(wins);
-            game()
-        }
-        //This is the loser's notice
-        function youLose() {
-            alert("You have lost!");
-            losses++;
-            $('#losses').text(losses);
-            game()
-        }
-    //These are the on click events/listeners when the user clicks the gems
-        $(.emeralds).on('click', function() {
-            userInitialScore = userInitialScore + randomEmeraldNumber;
-            console.log("Your score currently is " + userInitialScore);
-            $('#userFinalScore').text(userInitialScore);
-            if (userInitialScore === randomNumber) {
-                youWin();
-            else (userInitialScore > randomNumber){
-                youLose();
+        opalNumber = Math.floor(Math.random()*12 + 1)
+        var opals = $('#opals');
+            opals.attr('data-gemValue', opalNumber);
+
+        rubyNumber = Math.floor(Math.random()*12 + 1)
+        var rubies = $('#rubies');
+            rubies.attr('data-gemValue', rubyNumber);
+
+        tigerEyeNumber = Math.floor(Math.random()*12 + 1)
+        var tigersEyes = $('#tigersEyes');
+            tigersEyes.attr('data-gemValue', tigerEyeNumber);
+
+     //This is the function / event listener when the user clicks the gems
+        $('.img-thumbnail').on('click', function() {
+                var gemValue = ($(this).attr('data-gemValue'));
+                gemValue = parseInt(gemValue);
+                userScore += gemValue;
+            $('#userScore').text(userScore);
+            if (userScore === randomNumber) {
+                $('#alert').append("You've won !");
+                wins ++;
+                userScore = 0;
+                $('#winScore').text(wins);
             }
-        })
-        $(.opals).on('click', function() {
-            userInitialScore = userInitialScore + randomOpalNumber;
-            console.log("Your score currently is " + userInitialScore);
-            $('#userFinalScore').text(userInitialScore);
-            if (userInitialScore === randomNumber) {
-                youWin();
-            else (userInitialScore > randomNumber){
-                youLose();
-            }
-        })
-        $(.rubies).on('click', function() {
-            userInitialScore = userInitialScore + randomRubyNumber;
-            console.log("Your score currently is " + userInitialScore);
-            $('#userFinalScore').text(userInitialScore);
-            if (userInitialScore === randomNumber) {
-                youWin();
-            else (userInitialScore > randomNumber){
-                youLose();
-            }
-        })
-        $(.tigersEyes).on('click', function() {
-            userInitialScore = userInitialScore + randomTigerEyeNumber;
-            console.log("Your score currently is " + userInitialScore);
-            $('#userFinalScore').text(userInitialScore);
-            if (userInitialScore === randomNumber) {
-                youWin();
-            else (userInitialScore > randomNumber){
-                youLose();
+            else if (userScore > randomNumber) {
+                $('#alert').append("You've lost!");
+                losses ++;
+                userScore = 0;
+                $('#lossScore').text(losses);
             }
         });
-});
+    });
+}); 
