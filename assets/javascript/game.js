32ccf0c5b656842cf0031296db37502d04324775
Mona Lisa Mameli <lisa.noetzel@gmail.com>
2019-08-11 13:22:49 -0400

    final commit. all bugs fixed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,55 +12,73 @@ $(document).ready(function(){
         var rubyNumber = 0;
         var tigerEyeNumber = 0;
 
-    //This is the game start function / on click listener for the Start Button  
-    $('.btn-block').on('click', function() {
+        var randomNumber;
 
+    //New game function
+        function newGame() {
+            
     //This generates the computer's random numbers from 19 - 120 
-        var randomNumber = Math.floor(Math.random()*102 + 19)
-            console.log("this generates a random number:" + randomNumber);
+        randomNumber = Math.floor(Math.random()*102 + 19)
+        console.log("this generates a random number:" + randomNumber);
+        userScore = 0;
 
     //This displays the scores for the number to tie, wins, losses, and an alert
         $('#scoreToTie').text(randomNumber);
         $('#winScore').text(wins);
         $('#lossScore').text(losses);
         $('#alert').empty();
+        $('#userScore').text(userScore);
 
-    //This generates random numbers from 1-12 for each of the four gems
-        emeraldNumber = Math.floor(Math.random()*12 + 1)
-            console.log("These are random numbers for the emeralds:" + emeraldNumber)
-        var emeralds = $('#emeralds');
-            emeralds.attr("data-gemValue", emeraldNumber);
+    // //This generates random numbers from 1-12 for each of the four gems
+    //     emeraldNumber = Math.floor(Math.random()*12 + 1)
+    //         console.log("These are random numbers for the emeralds:" + emeraldNumber)
+    //     var emeralds = $('#emeralds');
+    //         emeralds.attr("data-gemValue", emeraldNumber);
 
-        opalNumber = Math.floor(Math.random()*12 + 1)
-        var opals = $('#opals');
-            opals.attr('data-gemValue', opalNumber);
+    //     opalNumber = Math.floor(Math.random()*12 + 1)
+    //     var opals = $('#opals');
+    //         opals.attr('data-gemValue', opalNumber);
 
-        rubyNumber = Math.floor(Math.random()*12 + 1)
-        var rubies = $('#rubies');
-            rubies.attr('data-gemValue', rubyNumber);
+    //     rubyNumber = Math.floor(Math.random()*12 + 1)
+    //     var rubies = $('#rubies');
+    //         rubies.attr('data-gemValue', rubyNumber);
 
-        tigerEyeNumber = Math.floor(Math.random()*12 + 1)
-        var tigersEyes = $('#tigersEyes');
-            tigersEyes.attr('data-gemValue', tigerEyeNumber);
+    //     tigerEyeNumber = Math.floor(Math.random()*12 + 1)
+    //     var tigersEyes = $('#tigersEyes');
+    //         tigersEyes.attr('data-gemValue', tigerEyeNumber);
+     
+    //jQuery each method to cut down on repetitive coding - this loops over the class img-thumbnail
+            $('.img-thumbnail').each(function() {
+                var random = Math.floor(Math.random()*12 + 1)
+                $(this).attr('data-gemValue', random);
+            })
+        }
 
-     //This is the function / event listener when the user clicks the gems
-        $('.img-thumbnail').on('click', function() {
-                var gemValue = ($(this).attr('data-gemValue'));
-                gemValue = parseInt(gemValue);
-                userScore += gemValue;
-            $('#userScore').text(userScore);
-            if (userScore === randomNumber) {
-                $('#alert').append("You've won !");
-                wins ++;
-                userScore = 0;
-                $('#winScore').text(wins);
-            }
-            else if (userScore > randomNumber) {
-                $('#alert').append("You've lost!");
-                losses ++;
-                userScore = 0;
-                $('#lossScore').text(losses);
-            }
-        });
-    });
-}); 
+    //This is the game start function / on click listener for the Start Button  
+    $('.btn-block').on('click', newGame)
+
+      //This is the function / event listener when the user clicks the gems
+      $('.img-thumbnail').on('click', function() {
+
+        //this prevents NaN appearing when clicking on gem image
+        if (!randomNumber) {
+            return false
+        }
+        var gemValue = ($(this).attr('data-gemValue'));
+        gemValue = parseInt(gemValue);
+        userScore += gemValue;
+    $('#userScore').text(userScore);
+    if (userScore === randomNumber) {
+        $('#alert').append("You've won !");
+        wins ++;
+        newGame();
+    }
+    else if (userScore > randomNumber) {
+        $('#alert').append("You've lost!");
+        losses ++;
+        newGame();
+    }
+});
+    
+});
+
